a8e05730b05772f412850a66fd115c4cb04b59e8
Dmitry Volevodz <dimavolevodz@gmail.com>
2017-11-15 14:39:50 +0300

    Added Arrays - Duplicates

---
+++ Arrays/Duplicates/Solution.playground/Contents.swift
@@ -0,0 +1,34 @@
+/*
+ Remove Duplicates from Sorted Array
+     Given a sorted array, remove the duplicates in-place such that each element appear only once and return the new length.
+     Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+     Example:
+     Given nums = [1,1,2],
+     Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+     It doesn't matter what you leave beyond the new length.
+ */
+
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+    if nums.count <= 0 {
+        return 0
+    }
+    var index = 1
+    
+    for i in 1 ..< nums.count {
+        if nums[i] != nums[i-1] {
+            nums[index] = nums[i]
+            index += 1
+        }
+    }
+    return index
+}
+
+
+var a: Array<Int> = [1, 1, 2, 2, 3, 4, 5, 6, 6, 7, 7, 8]
+
+print(removeDuplicates(&a))
+
+print(a)
+
+
+
--- Arrays/README.md
+++ Arrays/README.md
@@ -2,8 +2,9 @@
 
 This folder contains array problems.
 
-* [Matrix Rotation](/Arrays/MatrixRotation/)
-* [Zero Matrix](/Arrays/ZeroMatrix/)
+* [Matrix Rotation](/Arrays/MatrixRotation/)  
+* [Zero Matrix](/Arrays/ZeroMatrix/)  
 * [Two Sum](/Arrays/TwoSum/)  
+* [Remove Duplicates from Sorted Array](/Arrays/Duplicates/)  
 
 [//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
