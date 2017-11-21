053146a7185db1e9ec33c6c1c162bccdfee4bc8d
Dmitry Volevodz <dimavolevodz@gmail.com>
2017-11-21 19:08:04 +0300

    Added Math - Stairs

---
+++ Math/Stairs/README.md
@@ -0,0 +1,12 @@
+#  Climbing Stairs
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Note: Given n will be a positive integer.
+    
+# Solutions:
+
+# Swift
+`O(n)`
+
+# Installation
+Run `make` to build and run Swift files. Or you can run `.playground` file.
---
+++ Math/Stairs/Solution.swift
@@ -0,0 +1,24 @@
+/*
+  Climbing Stairs
+    You are climbing a stair case. It takes n steps to reach to the top.
+    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    Note: Given n will be a positive integer.
+*/
+
+func climbStairs(_ n: Int) -> Int {
+    
+    if n == 1 || n == 2 {
+        return n
+    }
+    
+    var first = 1
+    var second = 2
+    var third = 0
+    
+    for _ in 3 ..< n + 1 {
+        third = first + second
+        first = second
+        second = third
+    }
+    return third
+}
