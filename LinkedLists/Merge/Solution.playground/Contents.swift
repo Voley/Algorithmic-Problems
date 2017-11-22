7c92f3a2f62aeae86a0599f1e4335be9457e63cd
Dmitry Volevodz <dimavolevodz@gmail.com>
2017-11-22 14:53:04 +0300

    Added Linked List - Merge

---
+++ LinkedLists/Merge/Solution.playground/Contents.swift
@@ -0,0 +1,54 @@
+/*
+ Merge Two Sorted Lists
+    Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+*/
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    var first: ListNode? = l1
+    var second: ListNode? = l2
+    var head: ListNode?
+    var tail: ListNode?
+    
+    while first != nil || second != nil {
+        guard let f = first else {
+            guard let s = second else { continue }
+            appendNodeToList(&head, &tail, s)
+            second = s.next
+            continue
+        }
+        guard let s = second else {
+            guard let f = first else { continue }
+            appendNodeToList(&head, &tail, f)
+            first = f.next
+            continue
+        }
+        if f.val > s.val {
+            appendNodeToList(&head, &tail, s)
+            second = s.next
+        } else {
+            appendNodeToList(&head, &tail, f)
+            first = f.next
+        }
+    }
+    tail?.next = nil
+    return head
+}
+
+func appendNodeToList(_ head: inout ListNode?, _ tail: inout ListNode?, _ node: ListNode) {
+    if tail != nil {
+        tail?.next = node
+        tail = node
+    } else {
+        head = node
+        tail = node
+    }
+}
--- LinkedLists/README.md
+++ LinkedLists/README.md
@@ -11,5 +11,6 @@ This folder contains Linked List problems.
 * [Intersection](/LinkedLists/Intersection/)
 * [Loop Detection](/LinkedLists/LoopDetection/)
 * [Merge k Sorted Lists](MergeK)  
+* [Merge Two Sorted Lists](Merge)  
 
 [//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
